ca603e3727c8a1b84e02e7d3ba231292bcfe3f37
Sanjay <sanzay.369@gmail.com>
2024-06-03 22:23:40 +0545

    inheritance

--- programs/inheritance/inheritance.py
+++ programs/inheritance/inheritance.py
@@ -21,3 +21,28 @@ cat = Cat("Catty")
 
 print(f"{dog.name} says: {dog.speak()}")
 print(f"{cat.name} says: {cat.speak()}")
+
+
+# another example of inheritance
+
+class Person:
+    def __init__(self, name, age, address):
+        self.name = name
+        self.age = age
+        self.address = address
+
+    def __str__(self):
+        return f"Name: {self.name}, Age: {self.age}, Address: {self.address}"
+
+
+class Student(Person):
+    def __init__(self, name, age, address, faculty):
+        self.faculty = faculty
+        super().__init__(name, age, address)
+
+    def __str__(self):
+        return f"{super().__str__()}, faculty: {self.faculty}"
+
+
+student = Student("sanjay", 23, "Kathmandu", "BCA")
+print(student)
--- programs/program.py
+++ programs/program.py
@@ -1,3 +1,19 @@
 # this is for testing the programs
 
-print("hello world")
+class Student:
+    def __init__(self, phy, chem, math):
+        self.phy = phy
+        self.chem = chem
+        self.math = math
+        # self.percentage = str((self.phy + self.chem + self.math) / 3) + "%"
+
+    @property
+    def percentage(self):
+        return str((self.phy + self.chem + self.math) / 3) + "%"        
+
+
+stu = Student(98, 97, 99)
+print(stu.percentage)
+
+stu.phy = 86
+print(stu.percentage)
