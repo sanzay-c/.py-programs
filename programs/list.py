5bc6cae92d4c7272f1f180f89cfa3c25aca545ff
Sanjay <sanzay.369@gmail.com>
2024-06-04 23:58:57 +0545

    adding the elements of list

--- programs/list.py
+++ programs/list.py
@@ -6,3 +6,15 @@ myList = [5, 8, 7, 2, 9]
 print(myList.append(1)) # adds and put it in the last
 print(myList.sort())
 print(myList) 
+
+# function to add a tolal elements of list (python class question answer)
+
+def sum_of_list(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+numbers = [1, 2, 3, 4, 5, 6, 6]
+total = sum_of_list(numbers)
+print(total)
--- programs/program.py
+++ programs/program.py
@@ -1,19 +1,59 @@
 # this is for testing the programs
 
-class Student:
-    def __init__(self, phy, chem, math):
-        self.phy = phy
-        self.chem = chem
-        self.math = math
-        # self.percentage = str((self.phy + self.chem + self.math) / 3) + "%"
+# class Student:
+#     def __init__(self, phy, chem, math):
+#         self.phy = phy
+#         self.chem = chem
+#         self.math = math
+#         # self.percentage = str((self.phy + self.chem + self.math) / 3) + "%"
 
-    @property
-    def percentage(self):
-        return str((self.phy + self.chem + self.math) / 3) + "%"        
+#     @property
+#     def percentage(self):
+#         return str((self.phy + self.chem + self.math) / 3) + "%"        
 
 
-stu = Student(98, 97, 99)
-print(stu.percentage)
+# stu = Student(98, 97, 99)
+# print(stu.percentage)
 
-stu.phy = 86
-print(stu.percentage)
+# stu.phy = 86
+# print(stu.percentage)
+
+
+# numbers = [1, 2, 10, 14, 8, 3]
+
+# max_number = numbers[0]
+
+# for num in range(len(numbers)):
+#     if numbers[num] > max_number:
+#         max_number = numbers[num]
+
+# print(f"the max number in the array list is {max_number}")
+
+# from cmath import inf
+
+# numbers = [1, 2, 10, 14, 8, 3]
+
+# array_length = len(numbers)
+# largest_element = float(-inf)
+
+# for i in range(array_length):
+#     element = numbers[i]
+
+#     if element > largest_element:
+#         largest_element = element
+
+# print(f"the largest element in the array is: {largest_element}")
+
+
+
+# array = [1, 2, 3, 2, 1]
+
+# new_array = array.copy()
+
+# new_array.reverse()
+
+# if array == new_array:
+#     print("This is the palindrome number")
+# else:
+#     print("This is not the palindrome number")
+
