f78ba91f0f683d03ac7d296b274a935620c7a82c
Sanjay <sanzay.369@gmail.com>
2024-06-09 23:16:47 +0545

    merge two dict into one

---
+++ programs/pthon_class_qna/merge_two_dict.py
@@ -0,0 +1,13 @@
+dict1 = {
+    "name" : "Ram",
+    "age" : 23,
+}
+
+dict2 = {
+    "address" : "Kathmandu",
+    "hobbies" : ['cricket','football', 'tennis']
+}
+
+dict1.update(dict2) # using .update method to merge two dict into one 
+
+print(dict1)
