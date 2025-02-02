3bf5abf18189bbbbb03a3cfbec99200f19c4ca54
Sanjay <sanzay.369@gmail.com>
2025-02-02 17:27:00 +0545

    model saved

--- sklearn/house_price_datasets/house_price_st.py
+++ sklearn/house_price_datasets/house_price_st.py
@@ -4,10 +4,10 @@ import pickle
 import os
 
 # Load the trained model and scaler
-with open(os.path.join(os.path.dirname(__file__),'trained_model.pkl'), 'rb') as file:
+with open(os.path.join(os.path.dirname(__file__),'model_saved/trained_model.pkl'), 'rb') as file:
     loaded_model = pickle.load(file)
 
-with open(os.path.join(os.path.dirname(__file__),'scaler.pkl'), 'rb') as file:
+with open(os.path.join(os.path.dirname(__file__),'model_saved/scaler.pkl'), 'rb') as file:
     scaler = pickle.load(file)
 
 # Streamlit app
